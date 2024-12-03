bc5458e662806d89cb4443817a412b228cd84349
Sean Dooley <s.dooley@ctidigital.com>
2024-12-03 11:06:15 +0000

    Initial Commit

---
+++ swift100/swift100App.swift
@@ -0,0 +1,17 @@
+//
+//  swift100App.swift
+//  swift100
+//
+//  Created by Sean Dooley on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
